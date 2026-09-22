4b86522f8a43407ee9906a4a012aefe30f77e0b7
Daoyuan Li <94409450+DaoyuanLi2816@users.noreply.github.com>
2026-09-21 23:24:55 -0700

    fix: align LoRA inference paths with training output

--- eval_llm.py
+++ eval_llm.py
@@ -23,7 +23,7 @@ def init_model(args):
         model.load_state_dict(torch.load(ckp, map_location=args.device), strict=True)
         if args.lora_weight != 'None':
             apply_lora(model)
-            load_lora(model, f'./{args.save_dir}/{args.lora_weight}_{args.hidden_size}.pth')
+            load_lora(model, f'./{args.save_dir}/{args.lora_weight}_{args.hidden_size}{moe_suffix}.pth')
     else:
         model = AutoModelForCausalLM.from_pretrained(args.load_from, trust_remote_code=True)
     get_model_params(model, model.config)
